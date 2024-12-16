46170a730eb72556d5b108b3b56b58b93bc9205d
shade477 <sagardeyof477@gmail.com>
2024-12-17 02:03:34 +0530

    Dependency injection creates and injects the object at the constructor

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -4,6 +4,7 @@ import com.example.demo.dao.StudentDAO;
 import com.example.demo.dao.StudentDAOImpl;
 import com.example.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //import java.util.ArrayList;
@@ -15,6 +16,11 @@ public class StudentService {
     private List<Student> theStudents;
     private StudentDAO studentDAO;
 
+    @Autowired
+    public StudentService(StudentDAOImpl theStudentDAO){
+        studentDAO = theStudentDAO;
+    }
+
     // define @PostConstruct to load the student data ... only once!
     @PostConstruct
     private void loadData() {
@@ -22,10 +28,6 @@ public class StudentService {
         studentDAO.save(new Student("Poornima", "Patel"));
         studentDAO.save(new Student("Mario", "Rossi"));
         studentDAO.save(new Student("Mary", "Smith"));
-    }
-
-    public StudentService() {
-        this.studentDAO = new StudentDAOImpl();
     }
 
     public String getName(int id) {
