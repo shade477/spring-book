d9647c3f4cc9fd77c9b90fff34365ff731a6d0ba
shade477 <sagardeyof477@gmail.com>
2024-12-30 02:36:56 +0530

    Error at CourseDAO

--- src/main/java/com/example/demo/dao/CourseDAO.java
+++ src/main/java/com/example/demo/dao/CourseDAO.java
@@ -1,8 +1,10 @@
-package com.example.demo.dao;
-
-import com.example.demo.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDAO extends JpaRepository<Course, Integer> {
-    // JPARepository will create basic JPA functions
-}
+//package com.example.demo.dao;
+//
+//import com.example.demo.entity.Course;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface CourseDAO extends JpaRepository<Course, Integer> {
+//    // JPARepository will create basic JPA functions
+//}
